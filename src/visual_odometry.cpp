6ee9a6afe3da62cd236ed2861e1024a2d9b25bb6
AMNshaw <rosel94638@gmail.com>
2025-03-08 17:17:00 +0800

    Add some comment

--- src/visual_odometry.cpp
+++ src/visual_odometry.cpp
@@ -6,9 +6,9 @@ namespace vo_husky {
 VisualOdometry::VisualOdometry(Camera::Ptr camera) : camera_(camera){}
 
 bool VisualOdometry::Init() {
-    // read from config file
 
     // create components and links
+    
     frontend_ = std::make_shared<Frontend>();
     backend_ = std::make_shared<Backend>();
     map_ = std::make_shared<Map>();
@@ -29,18 +29,18 @@ bool VisualOdometry::Init() {
 }
 
 bool VisualOdometry::step(Frame::Ptr newFrame){
-
+    // Next frame
     auto t1 = std::chrono::steady_clock::now();
     bool success = frontend_->AddFrame(newFrame);
     auto t2 = std::chrono::steady_clock::now();
     auto time_used = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-    // LOG(INFO) << "VO cost time: " << time_used.count() << " seconds.";
+    LOG(INFO) << "VO cost time: " << time_used.count() << " seconds.";
     return success;
 }
 
 
 void VisualOdometry::End() {
-
+    // Close the threads of backend and viwer
     backend_->Stop();
     viewer_->Close();
 
